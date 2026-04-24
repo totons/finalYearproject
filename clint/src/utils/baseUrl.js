b5e31d30372d7d4c369124af047d7deebc17673c
oveshak <oveshaktoton9999@gmail.com>
2026-04-25 00:47:10 +0600

    add file

--- clint/src/utils/baseUrl.js
+++ clint/src/utils/baseUrl.js
@@ -1,4 +1,4 @@
 export const getBaseUrl =()=>{
-    //return "https://final-yearproject-8pi5.vercel.app"
-    return "http://127.0.0.1:5000"   
+    return "https://final-yearproject-8pi5.vercel.app"
+    //return "http://127.0.0.1:5000"   
 }
