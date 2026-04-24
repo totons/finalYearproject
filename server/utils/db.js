104cf62a411009d730a7b66a8bb810607d584b12
oveshak <oveshaktoton9999@gmail.com>
2026-04-24 06:20:22 +0600

    add file

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,17 +1,51 @@
+// // import mongoose from "mongoose";
+
+// // const connectdb = async() => {
+// //     try {
+// //         await mongoose.connect(process.env.mongo_url, {
+// //             serverSelectionTimeoutMS: 30000, // 30 seconds
+// //         });
+// //         console.log("MongoDB Connected!");
+// //     } catch (err) {
+// //         console.error(err);
+
+// //     }
+// // }
+// // export default connectdb
 // import mongoose from "mongoose";
 
-// const connectdb = async() => {
-//     try {
-//         await mongoose.connect(process.env.mongo_url, {
-//             serverSelectionTimeoutMS: 30000, // 30 seconds
-//         });
-//         console.log("MongoDB Connected!");
-//     } catch (err) {
-//         console.error(err);
+// let isConnected = false;
 
+// const connectdb = async () => {
+//   try {
+//     if (isConnected) {
+//       console.log("MongoDB already connected");
+//       return;
 //     }
-// }
-// export default connectdb
+
+//     const mongoUri = process.env.MONGO_URI || process.env.mongo_url;
+
+//     if (!mongoUri) {
+//       throw new Error("MongoDB URI missing. Add MONGO_URI in Vercel environment variables.");
+//     }
+
+//     const db = await mongoose.connect(mongoUri, {
+//       serverSelectionTimeoutMS: 10000,
+//     });
+
+//     isConnected = db.connections[0].readyState === 1;
+
+//     console.log("MongoDB connected successfully");
+//   } catch (error) {
+//     console.error("MongoDB connection failed:", error.message);
+//     throw error;
+//   }
+// };
+
+// export default connectdb;
+
+
+
 import mongoose from "mongoose";
 
 let isConnected = false;
@@ -26,7 +60,7 @@ const connectdb = async () => {
     const mongoUri = process.env.MONGO_URI || process.env.mongo_url;
 
     if (!mongoUri) {
-      throw new Error("MongoDB URI missing. Add MONGO_URI in Vercel environment variables.");
+      throw new Error("MongoDB URI missing. Add MONGO_URI in Vercel.");
     }
 
     const db = await mongoose.connect(mongoUri, {
